c17ee25c5074ddd7767966bed3cf75faa37325fe
MarcoBran <114394974+MarcoBran@users.noreply.github.com>
2022-10-27 11:57:42 +0200

    Add files via upload

---
+++ d4/assets/js/script.js
@@ -0,0 +1,14 @@
+const title = document.querySelector("#title");
+
+function changeColor(){
+    title.style.color = "red";
+}
+function changeSize(){
+    title.style.fontSize = "100px";
+}
+function changeCase(){
+    title.style.textTransform = "uppercase"
+}
+function changeColor2(){
+    title.style.color = "white";
+}
